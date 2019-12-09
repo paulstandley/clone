c146760fb85a0638490f8ca0de411bc7e728e106
Paul Standley <paulstandley1972@gmail.com>
2019-12-09 20:55:50 +0000

    add svg and header styl sheet

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Clone') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -23,9 +23,12 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
+                <header class="class-header">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        <img class="img" src="images/iconfinder.svg" alt="start wars stomtrooper">
+                         <h1>{{ config('app.name', 'Clone') }}</h1>
+                    </a>
+                </header>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
