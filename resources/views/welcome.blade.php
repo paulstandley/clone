8cdc2d937a9b9dd150cc3d157eae8849b0e1befe
Paul Standley <paulstandley1972@gmail.com>
2019-12-10 21:54:38 +0000

    style welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,66 +4,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{ config('app.name', 'Clone') }}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
-    <body>
+    <body id="welcome">
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
@@ -81,18 +30,18 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    <header>
+                            <img class="welcome-img" src="images/iconfinder.svg" alt="start wars stomtrooper">
+                        <h1>Clone</h1>
+                    </header>
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a target="_blank" href="https://paulstandley.com">Paul Standley</a>
+                    <a target="_blank" href="https://www.coderlist.io/profile/paulstandley?sc=zpo6XTRcVhJTZMweY8Fkqix3">Codelist</a>
+                    <a target="_blank" href="https://codepen.io/pielo2">Codepen</a>
+                    <a target="_blank" href="https://github.com/paulstandley/">GitHub</a>
+                    <a target="_blank" href="https://www.iconfinder.com/icons/216965/clone_storm_trooper_icon">Iconfinder</a>
                 </div>
             </div>
         </div>
