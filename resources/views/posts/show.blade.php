dfa8f7f3d03e2f185cf28bf2e1f43232e39ac01a
Paul Standley <paulstandley1972@gmail.com>
2020-01-01 15:11:16 +0000

    add user caption and name to show

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,8 +2,16 @@
 
 @section('content')
 <div class="container">
-  <section class="post_section p-4">
-    <img class="w-100" src="/storage/{{  $post->image  }}" alt="profile image">
-  </section>
+  <div class="row post_section p-4">
+    <div class="col-8">
+      <img class="w-100" src="/storage/{{  $post->image  }}" alt="your image">
+    </div>
+    <div class="col-4">
+      <div>
+        <h3>{{  $post->user->username  }}</h3>
+        <p>{{  $post->caption  }}</p>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
